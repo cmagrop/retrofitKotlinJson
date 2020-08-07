05bfefe75765053e3cff248b1c2b26ee0d7a2a86
Christian Magro <cmagrop@gmail.com>
2020-08-07 12:29:41 -0400

    Implementacion a prueba

--- app/src/main/java/com/example/practicaretrofit/DataModel.kt
+++ app/src/main/java/com/example/practicaretrofit/DataModel.kt
@@ -9,15 +9,19 @@ data class DataModel (
      @Expose
      @SerializedName("postId")
      val postId: Integer,
+
      @Expose
      @SerializedName("id")
      val id: Integer,
+
      @Expose
      @SerializedName("name")
      val name: String,
+
      @Expose
      @SerializedName("email")
-     val email : String,
+     val email: String,
+
      @Expose
      @SerializedName("body")
      val body: String
